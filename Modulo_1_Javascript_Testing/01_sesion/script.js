8e2c5d771e09e3f3ebc96b1603384dc475d0afa5
jamorenog <jmoreno@megacable.com.mx>
2025-03-24 17:06:22 -0600

    Sesion 1 Jorge Moreno
    Sesion 1 Jorge Moreno - jmoreno@megacable.com.mx

---
+++ Modulo_1_Javascript_Testing/01_sesion/script.js
@@ -0,0 +1,21 @@
+//alert("Hola a todos, bienvenidos");
+
+// Declaración de variables 
+var int = 12345;
+var string = "texto";
+var boolean = true;
+
+var nombre = "Jorge";
+var apellido = "Moreno";
+var nombreCompleto = `${nombre} ${apellido}`
+var edad = 55;
+
+console.log(`Hola, soy ${nombreCompleto} y tengo ${edad} años`);
+console.log("Hola, soy "+ nombreCompleto + " y tengo "+ edad + " años");
+
+
+//Comparación 
+// ==, ===, !=, !==, <, >, >=, <=
+var numero = 7
+var numeroTexto = "25";
+console.log(numero - numeroTexto);
