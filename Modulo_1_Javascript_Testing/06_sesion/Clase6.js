849faae2f17428746b131f057bf37009db79a48b
jamorenog <jmoreno@megacable.com.mx>
2025-04-01 18:12:21 -0600

    Clase 6 Jorge Moreno

---
+++ Modulo_1_Javascript_Testing/06_sesion/Clase6.js
@@ -0,0 +1,20 @@
+// const usuario = {
+//     nombre: "Ana",
+//     edad: 30,
+//     ciudad: "Madrid"
+// }
+
+// const usuarioJson = JSON.stringify(usuario);
+// console.log("JSON en texto: ", usuarioJson);
+
+// // const usuarioObjeto = JSON.parse(usuarioJson);
+// // console.log("JSON en objeto: ", usuarioJson);
+
+
+// localStorage.setItem("usuario", usuarioJson);
+
+// const datosGuardados = localStorage.getItem("usuario");
+// const datosObjeto= JSON.parse(datosGuardados);
+// console.log(datosObjeto);
+
+
