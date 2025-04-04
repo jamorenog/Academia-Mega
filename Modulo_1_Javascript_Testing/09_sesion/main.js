863768ea8bad6f0ca308ac41cd88caaa2667064a
jamorenog <jmoreno@megacable.com.mx>
2025-04-04 08:34:05 -0600

    Clase 8 y 9

---
+++ Modulo_1_Javascript_Testing/09_sesion/main.js
@@ -0,0 +1,13 @@
+//import {saludar, despedir,suma} from "./saludo.js";
+import {suma, resta, multiplicar, dividir} from "./Actividad.js";
+
+// const nombre = "Jorge Moreno";
+
+// console.log(saludar(nombre));
+// console.log(despedir(nombre));
+
+
+console.log(suma(10,5))
+console.log(resta(10,5))
+console.log(multiplicar(10,5))
+console.log(dividir(10,5))
