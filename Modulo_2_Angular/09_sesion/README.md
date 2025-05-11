cd992b6c8601f99fe2e9335effc3a0a901bad1c2
jamorenog <jmoreno@megacable.com.mx>
2025-05-11 13:44:36 -0600

    Clase 9 jmoreno

---
+++ Modulo_2_Angular/09_sesion/README.md
@@ -0,0 +1,13 @@
+# Clase 9: 
+Comunicación entre Componentes
+
+# Fecha: 
+02/05/2025
+
+# Objetivos
+    - Aprender a compartir datos entre componentes usando @input() y @output.
+
+# Actividades
+    - IF y else con NG Templates
+    
+
---
+++ Modulo_2_Angular/09_sesion/src/app/app.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { Saludo } from './Components/Saludo/saludo.components';
+import { CardComponent } from './Components/card/card.component';
+import { TarjetaComponent } from './Components/tarjeta/tarjeta.component';
+import { CommonModule } from '@angular/common';
+import { TodoComponent } from './Components/todo/todo.component';
+import { ProductManagerComponent } from './Components/product-manager/product-manager.component';
+import { DetallesComponent } from './Components/detalles/detalles.component';
+
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, 
+    Saludo, CardComponent, 
+    TarjetaComponent, CommonModule, 
+    TodoComponent, ProductManagerComponent, 
+    RouterLink, DetallesComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+
+export class AppComponent {
+
+  rolUsuario = 'cliente';
+
+  tareaImportante = true;
+
+  isUrgente = false;
+}
